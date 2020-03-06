40cfb04745a8e9346711b90d562bce7f077d1f32
Emre GOREN <emregrnn58@gmail.com>
2020-03-06 13:58:23 +0300

    MqttExample

---
+++ app/src/main/java/com/emregoren/mqttexample/MainActivity.kt
@@ -0,0 +1,135 @@
+package com.emregoren.mqttexample
+
+import android.content.Context
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+import org.eclipse.paho.android.service.MqttAndroidClient
+import org.eclipse.paho.client.mqttv3.*
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var mqttAndroidClient: MqttAndroidClient
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        button.setOnClickListener {
+            connect(this)
+
+        }
+
+    }
+    fun connect(applicationContext : Context) {
+        mqttAndroidClient = MqttAndroidClient ( applicationContext,"tcp://192.168.43.122:1883","60" )
+        try {
+            val token = mqttAndroidClient.connect()
+            token.actionCallback = object : IMqttActionListener {
+                override fun onSuccess(asyncActionToken: IMqttToken)                        {
+                    Toast.makeText(applicationContext,"baglandı",Toast.LENGTH_LONG).show()
+                    //connectionStatus = true
+                    // Give your callback on connection established here
+                    println("emre")
+                    publish("emre","selamun aleykum")
+
+                }
+                override fun onFailure(asyncActionToken: IMqttToken, exception: Throwable) {
+                    //connectionStatus = false
+                    Toast.makeText(applicationContext,"BAGLANTI YOK",Toast.LENGTH_LONG).show()
+                    // Give your callback on connection failure here
+                    println("emre"+exception.printStackTrace())
+
+                }
+            }
+        } catch (e: MqttException) {
+            // Give your callback on connection failure here
+            e.printStackTrace()
+        }
+    }
+    fun subscribe(topic: String) {
+        val qos = 2 // Mention your qos value
+        try {
+            mqttAndroidClient.subscribe(topic, qos, null, object : IMqttActionListener {
+                override fun onSuccess(asyncActionToken: IMqttToken) {
+                    // Give your callback on Subscription here
+                }
+                override fun onFailure(
+                    asyncActionToken: IMqttToken,
+                    exception: Throwable
+                ) {
+                    // Give your subscription failure callback here
+                }
+            })
+        } catch (e: MqttException) {
+            // Give your subscription failure callback here
+        }
+    }
+    fun unSubscribe(topic: String) {
+        try {
+            val unsubToken = mqttAndroidClient.unsubscribe(topic)
+            unsubToken.actionCallback = object : IMqttActionListener {
+                override fun onSuccess(asyncActionToken: IMqttToken) {
+                    // Give your callback on unsubscribing here
+                }
+                override fun onFailure(asyncActionToken: IMqttToken, exception: Throwable) {
+                    // Give your callback on failure here
+                }
+            }
+        } catch (e: MqttException) {
+            // Give your callback on failure here
+        }
+    }
+    fun receiveMessages() {
+        mqttAndroidClient.setCallback(object : MqttCallback {
+            override fun connectionLost(cause: Throwable) {
+                //connectionStatus = false
+                // Give your callback on failure here
+            }
+            override fun messageArrived(topic: String, message: MqttMessage) {
+                try {
+                    val data = String(message.payload, charset("UTF-8"))
+                    // data is the desired received message
+                    // Give your callback on message received here
+                } catch (e: Exception) {
+                    // Give your callback on error here
+                }
+            }
+            override fun deliveryComplete(token: IMqttDeliveryToken) {
+                // Acknowledgement on delivery complete
+            }
+        })
+    }
+    fun publish(topic: String, data: String) {
+        val encodedPayload : ByteArray
+        try {
+            encodedPayload = data.toByteArray(charset("UTF-8"))
+            val message = MqttMessage(encodedPayload)
+            message.qos = 2
+            message.isRetained = false
+            mqttAndroidClient.publish(topic, message)
+        } catch (e: Exception) {
+            // Give Callback on error here
+        } catch (e: MqttException) {
+            // Give Callback on error here
+        }
+    }
+    fun disconnect() {
+        try {
+            val disconToken = mqttAndroidClient.disconnect()
+            disconToken.actionCallback = object : IMqttActionListener {
+                override fun onSuccess(asyncActionToken: IMqttToken) {
+                    //connectionStatus = false
+                    // Give Callback on disconnection here
+                }
+                override fun onFailure(
+                    asyncActionToken: IMqttToken,
+                    exception: Throwable
+                ) {
+                    // Give Callback on error here
+                }
+            }
+        } catch (e: MqttException) {
+            // Give Callback on error here
+        }
+    }
+}
